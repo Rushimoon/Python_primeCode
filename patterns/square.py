25b4a1c7b3a4b9f3ba8837c4463b9574bfd96196
Rushimoon <rushikeshmoon1998@gmail.com>
2024-03-28 19:56:26 +0530

    update

---
+++ patterns/square.py
@@ -0,0 +1,27 @@
+# * * * * * * 
+# *         *
+# *         *
+# *         *
+# *         *
+# * * * * * * 
+
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+
+
+# 0-4
+# 1-4
+# 1 5 10 15
+
+"* * * * * * "  
+r=int(input("enter the row"))
+c=int(input("enter the col"))
+
+for z in range(1,r+1) :
+   b=""
+   for q in range(1,c+1):
+     b=b+"* "
+   print(b)  
